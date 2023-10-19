68ec171e52557e3af21b1c8eff423817b339f961
matter <139214654+matter4@users.noreply.github.com>
2023-10-19 10:43:56 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -54,11 +54,6 @@ window.addEventListener('DOMContentLoaded', event => {
     // Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
         elements: '#portfolio a.portfolio-box'
-    });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox2({
-        elements: '#portfolio2 a.portfolio-box'
-    });
+    }); 
 
 });
